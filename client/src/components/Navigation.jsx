b7118a4f04ef8f6d66a7618aa3a59b90f5c9c2a2
Johnny Watson <johnny@streetchange.org.uk>
2017-07-22 15:12:50 +0100

    Initial navbar styling added

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 class Navigation extends React.Component {
 	render() {
 		return (
-			<div className="navigation">This is the Navigation</div>
+			<div className="navigation">
+        <ul>
+          <li>Menu</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Snow</li>
+          <li>Surf</li>
+        </ul>
+        
+        <div>Stratis</div>
+        
+        <ul>
+          <li>Login</li>
+          <li>Search</li>
+          <li>Bag 0</li>
+        </ul>
+      </div>
 		)
 	}
 }
