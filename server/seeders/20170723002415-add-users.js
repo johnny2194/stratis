d9161ac86fb3081f89773c41b5adf6e1c7d01762
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-07-23 10:28:35 +0100

    add seeds for users

---
+++ server/seeders/20170723002415-add-users.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    
+      // Add altering commands here.
+      // Return a promise to correctly handle asynchronicity.
+
+      // Example:
+      return queryInterface.bulkInsert('Users', [
+        {
+          first_name: 'Eoghan',
+          last_name: 'Crowley',
+          email: 'eoghan@email.com',
+          password: '123dogs',
+          gender: 'male',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          first_name: 'Bob',
+          last_name: 'Peterson',
+          email: 'bobbyp@email.com',
+          password: 'bobby1962',
+          gender: 'male',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          first_name: 'Sue',
+          last_name: 'Davies',
+          email: 'sdavies@email.com',
+          password: 'sd1985',
+          gender: 'female',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], {});
+    
+  },
+
+  down: function (queryInterface, Sequelize) {
+    
+      // Add reverting commands here.
+      // Return a promise to correctly handle asynchronicity.
+
+      // Example:
+      return queryInterface.bulkDelete('Users', null, {});
+    
+  }
+};
