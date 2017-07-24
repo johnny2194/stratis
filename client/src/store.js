a6e0339ea40170d515d1544bf21d4efc7f754139
Johnny Watson <johnny@streetchange.org.uk>
2017-07-24 10:13:33 +0100

    Build out store with default state and browser history sync'd

--- client/src/store.js
+++ client/src/store.js
@@ -2,5 +2,20 @@ import { createStore } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { createBrowserHistory } from 'history/createBrowserHistory'
 
-const history = createBrowserHistory()
+import rootReducer from './reducers/index'
 
+const browserHistory = createBrowserHistory()
+
+const defaultState = {
+  navigation: { 
+    basketVisible: false,
+    menuVisible: false,
+    currentHoverDropdown: ""
+  }
+}
+
+const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+export const history = syncHistoryWithStore(browserHistory, store)
+
+export default store
