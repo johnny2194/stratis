5f83f0731179d3881ad0b7f32fdacefe0ec3851a
Johnny Watson <johnny@streetchange.org.uk>
2017-07-26 10:18:13 +0100

    Initial jsx structure and seeds updated

--- client/src/components/MensDropDown.jsx
+++ client/src/components/MensDropDown.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import * as actionCreators from '../actions/navigationActionCreators'
+import {Link} from 'react-router-dom'
+
 
 
 class MensDropDown extends React.Component {
@@ -27,7 +29,47 @@ class MensDropDown extends React.Component {
   render() {
     return (
       <div className="mens-drop-down">
-        <div className="menu"></div>
+        <div className="menu">
+          <div>
+            <Link to="/" className="menu-header">Board</Link>
+              <div className="link-column">
+                <Link to="/" className="sub-header">Hardware</Link>
+                <Link to="/" className="category-product-link">Snowboards</Link>
+                <Link to="/" className="category-product-link">Bindings</Link>
+                <Link to="/" className="category-product-link">Boots</Link>
+                <Link to="/" className="category-product-link">Helmets</Link>
+              </div>
+              <div className="link-column">
+                <Link to="/" className="sub-header">Gear</Link>
+                <Link to="/" className="category-product-link">Jackets</Link>
+                <Link to="/" className="category-product-link">Pants</Link>
+              </div>
+            </div>
+            <div>
+              <Link to="/" className="menu-header">Ski</Link>
+                <div className="link-column">
+                  <Link to="/" className="sub-header">Hardware</Link>
+                  <Link to="/" className="category-product-link">Skis</Link>
+                  <Link to="/" className="category-product-link">Bindings</Link>
+                  <Link to="/" className="category-product-link">Boots</Link>
+                  <Link to="/" className="category-product-link">Helmets</Link>
+                </div>
+                <div className="link-column">
+                  <Link to="/" className="sub-header">Gear</Link>
+                  <Link to="/" className="category-product-link">Jackets</Link>
+                  <Link to="/" className="category-product-link">Pants</Link>
+                </div>
+              </div>
+              <div>
+                <Link to="/" className="menu-header">Accessories</Link>
+                  <div className="link-column">
+                    <Link to="/" className="category-product-link">Socks</Link>
+                    <Link to="/" className="category-product-link">Beanies</Link>
+                    <Link to="/" className="category-product-link">Goggles</Link>
+                    <Link to="/" className="category-product-link">Gloves</Link>
+                  </div>
+              </div>
+        </div>
         <div ref="dropdown-background" onMouseEnter={this.handleHideDropdown.bind(this)}></div>
         
       </div>
